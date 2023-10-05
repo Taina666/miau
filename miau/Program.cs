1b6d92e7fc28b6e51c0ae672809bdc49f5dbe66c
Taina <bentopachecot@gmaail.com>
2023-10-05 15:51:49 -0400

    msg do commit

--- miau/Program.cs
+++ miau/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um numero: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite um numero: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        double result = num + num2;
+        Console.WriteLine($"resutado {result}");
+        Console.ReadKey();
+
+        if (num2 > num) Console.WriteLine($"O numero {num2} é maior");
+
+
+    }
+}
